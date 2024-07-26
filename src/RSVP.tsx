b268329d4dc500f0f8bb9318deaaeb68ee9dff79
Brandon Fite <bfite7@gatech.edu>
2024-07-25 21:09:40 -0600

    new images

--- src/RSVP.tsx
+++ src/RSVP.tsx
@@ -5,18 +5,19 @@ import { Link } from "react-router-dom";
 const RSVP = () => {
 
     return (
-        <div className="relative  text-block doodle-border pb-5">
-            {/* <div className="absolute top-0 left-0 w-16 h-16 border-none transform -rotate-[25deg]"><img className="border-none" src={oliImg}/></div> */}
-            {/* <div className="absolute bottom-0 right-0 transform translate-x-1/4  w-16 h-16"><img className="border-none" src={leafyStemImg}/></div> */}
+        <div className="relative text-block doodle-border pb-5 z-99">
 
-            <div className="doodle">
+            <div >
                 <h1 className="font-whimsical">RSVP</h1>
                 <></>
                 <p className="text-l">
                     Please get your RSVP in by September 12th!
                 </p>
                 <div className="pt-10">
-                    <Link to="/RSVP"  className="doodle-button bg-[#EFEFEF] doodle-border p-5">RSVP Here</Link>
+                    
+                    <div className="doodle">
+                        <Link to="/RSVP"  className="doodle-button bg-[#EFEFEF] doodle-border p-5">RSVP Here</Link>
+                    </div>
                 </div>
             </div>
 
--- src/Registry.tsx
+++ src/Registry.tsx
@@ -1,13 +1,21 @@
+import flyingplane from './assets/doodles/flyingplane.png'
+import polaroid from './assets/doodles/polaroids.png'
+import camera from './assets/doodles/camera.png'
+
 const Registry = () => {
     return (
-        <div className="text-block doodle-border">
-            <div>
+        <div className="relative text-block doodle-border z-10 flex justify-center items-center">
+            <div className="absolute left-1/4 transform inset-0 z-0 hidden md:block"><img className="h-full object-cover border-none" src={flyingplane}/></div>
+
+            <div className='flex flex-col items-center'>
                 <h1 className="font-whimsical">Registry</h1>
                 <p>
                     While your presence at our wedding is more than enough, if you wish to contribute to our honeymoon registry, please click the button below. Thank you!
                 </p>
-                <div className="p-10">
+                <div className="relative p-5 pt-10 max-w-[300px] mx-auto">
                     <a href="https://www.honeyfund.com/site/fite-certain-10-12-2024" className="doodle-button bg-[#EFEFEF] doodle-border p-3 no-underline text-black font-bold">Visit here!</a>
+                    <div className="absolute bottom-0 right-0 transform translate-x-full -translate-y-1 -rotate-[25deg] w-20 h-16"><img className="border-none" src={camera}/></div>
+                    <div className="absolute bottom-0 left-0 transform -translate-x-full translate-y-1/4 -rotate-[345deg] w-20 h-16"><img className="border-none" src={polaroid}/></div>
                 </div>
             </div>
         </div>
